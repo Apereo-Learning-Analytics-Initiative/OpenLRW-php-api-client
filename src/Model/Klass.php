dd2fb53411269927e07ee4b13fa76c89c985b666
xchopin <xchopin54@gmail.com>
2019-04-03 16:01:50 +0200

    feat: add Class model

---
+++ src/Model/Klass.php
@@ -0,0 +1,53 @@
+<?php
+
+/**
+ *
+ * Class entity
+ *
+ * @copyright Apereo
+ * @category OpenLRW
+ * @package  Entity
+ * @author   Xavier Chopin <bonjour@xavierchop.in>
+ * @license  http://www.osedu.org/licenses/ECL-2.0 ECL-2.0 License
+ */
+
+namespace OpenLRW\Model;
+
+
+class Klass extends OneRoster
+{
+    protected static $collection = 'classes';
+
+    // ToDo
+    protected $fillable = [
+        'sourcedId',
+        'status',
+        'metadata',
+        'lineItem'
+    ];
+
+    public static function enrollments(String $id)
+    {
+        return parent::get("classes/$id/enrollments");
+    }
+
+    public static function events(String $id)
+    {
+        return parent::get("classes/$id/events/stats");
+    }
+
+    public static function eventsForUser(String $id, String $userId)
+    {
+        return parent::get("classes/$id/events/user/$userId");
+    }
+
+    public static function resultsForUser(String $id, String $userId)
+    {
+        return parent::get("classes/$id/results/user/$userId");
+    }
+
+    public static function lineItems(String $id) {
+        return parent::get("classes/$id/lineitems");
+    }
+
+}
