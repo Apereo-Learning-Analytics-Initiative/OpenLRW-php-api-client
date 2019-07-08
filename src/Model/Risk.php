615e0aa8c865bd0845727e192b09890f90df7043
xchopin <xchopin54@gmail.com>
2019-07-08 10:36:20 +0200

    feat: add risk model

---
+++ src/Model/Risk.php
@@ -0,0 +1,44 @@
+<?php
+
+/**
+ *
+ * Class entity
+ *
+ * @copyright Apereo
+ * @category OpenLRW
+ * @package  Entity
+ * @author   Xavier Chopin <bonjour@xavierchop.in>
+ * @license  http://www.osedu.org/licenses/ECL-2.0 ECL-2.0 License
+ */
+
+namespace OpenLRW\Model;
+
+class Risk extends OneRoster
+{
+    protected static $collection = 'risk';
+
+    protected $fillable = [
+        'sourcedId',
+        'userSourcedId',
+        'classSourcedId',
+        'modelType',
+        'score',
+        'name',
+        'velocity',
+        'dateTime',
+        'active',
+        'metadata'
+    ];
+
+    public static function findByClassAndUser(String $classId, String $userId)
+    {
+        return parent::get("risks/classes/$classId/users/$userId", Risk::class);
+    }
+
+    public static function latestByClassAndUser(String $classId, String $userId)
+    {
+        return parent::get("risks/classes/$classId/users/$userId?date=latest", Risk::class);
+    }
+
+
+}
